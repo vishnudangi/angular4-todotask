e3572840e3d8ebe84fd3fe9da3e8faac93a8b01d
Coviam <coviam@Coviams-MacBook-Pro.local>
2017-11-07 17:49:51 +0530

    todo task edit delete and add task in angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TodoTaskComponent } from './components/todo-task/todo-task.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todo-task/todo-task.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-task',
+  templateUrl: './todo-task.component.html',
+  styleUrls: ['./todo-task.component.css'],
+})
+export class TodoTaskComponent implements OnInit {
+
+  todotaskList: any = ['aaa', 'ddd'];
+  taskInput: string;
+  isEditTask: Boolean = false;
+  editIndex: number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  addTask() {
+    console.log('addTask', this.taskInput);
+    if (this.taskInput) {
+      if (!!this.isEditTask) { // edit task
+        this.todotaskList.splice(this.editIndex, 1, this.taskInput);
+        this.taskInput = '';
+      } else {
+        this.todotaskList.push(this.taskInput);
+        this.taskInput = '';
+      }
+      this.isEditTask = false;
+    }
+  }
+  editTask(index: number): void {
+    console.log('editTask', index);
+    this.taskInput = this.todotaskList[index];
+    this.isEditTask = true;
+    this.editIndex = index;
+  }
+  deleteTask(index: number): void {
+    console.log('deleteTask', index);
+    this.todotaskList.splice(index, 1);
+  }
+}
